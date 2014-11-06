980b1e1defd879ece5a925431d75ce3fad9961e5
CheeseSucker <cheesesucker@gmail.com>
2014-11-06 05:17:52 +0100

    Added a few unit tests.

--- src/com/github/cheesesucker/sortvisualizer/tests/HelperTest.java
+++ src/com/github/cheesesucker/sortvisualizer/tests/HelperTest.java
@@ -1,5 +1,8 @@
 package com.github.cheesesucker.sortvisualizer.tests;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -33,5 +36,28 @@ public class HelperTest {
 	@Test
 	public void testLess_GreaterReturnsFalse() {
 		assertFalse(Helper.less(4, 3));
+	}
+	
+	@Test
+	public void testIsSorted_emptyReturnsTrue() {
+		assertTrue(Helper.isSorted(new double[]{}));
+	}
+	
+	@Test
+	public void testIsSorted_unsortedReturnsFalse() {
+		double[] input = new double[]{2, 1, 3};
+		assertFalse(Helper.isSorted(input));
+	}
+
+	@Test
+	public void testIsSorted_sortedReturnsTrue() {
+		double[] input = new double[]{0, 2, 3};
+		assertTrue(Helper.isSorted(input));
+	}
+	
+	@Test
+	public void testGenerateInput_correctSize() {
+		double[] actual = Helper.generateInput(3);
+		assertEquals(3, actual.length);
 	}
 }
